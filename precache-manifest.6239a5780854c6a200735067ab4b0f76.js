12d90fb5656f1eb76d9c529ff8728e41d6f22d1e
ekeCodes <mastmaula610@gmail.com>
2024-10-07 18:28:16 +0530

    Add files via upload

---
+++ precache-manifest.6239a5780854c6a200735067ab4b0f76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08324850ff99d669825d626426fd45d",
+    "url": "/fashion-cube/index.html"
+  },
+  {
+    "revision": "268c4e1721cbfa084007",
+    "url": "/fashion-cube/static/css/2.21900056.chunk.css"
+  },
+  {
+    "revision": "85ae31147118de388abc",
+    "url": "/fashion-cube/static/css/main.4513ddd2.chunk.css"
+  },
+  {
+    "revision": "268c4e1721cbfa084007",
+    "url": "/fashion-cube/static/js/2.8892b3d1.chunk.js"
+  },
+  {
+    "revision": "799df9b1668a25ec1cff125566e8bb1e",
+    "url": "/fashion-cube/static/js/2.8892b3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ae31147118de388abc",
+    "url": "/fashion-cube/static/js/main.e7c44b88.chunk.js"
+  },
+  {
+    "revision": "b6da803c473f3c570ef1",
+    "url": "/fashion-cube/static/js/runtime-main.a0d47386.js"
+  },
+  {
+    "revision": "fd4db0ff3f66f964fdb24f60ddab5a4a",
+    "url": "/fashion-cube/static/media/banner_1.fd4db0ff.jpg"
+  },
+  {
+    "revision": "f77e8fe9582f13fbd17999b876c1fe60",
+    "url": "/fashion-cube/static/media/banner_2.f77e8fe9.jpg"
+  },
+  {
+    "revision": "67bb10f2c266820f7f68e150896f87bc",
+    "url": "/fashion-cube/static/media/banner_3.67bb10f2.jpg"
+  },
+  {
+    "revision": "18a6bbdff932ced377d8031490bd3555",
+    "url": "/fashion-cube/static/media/deal_ofthe_week.18a6bbdf.png"
+  },
+  {
+    "revision": "0280082048c8b0a72b8a7bfd582d114b",
+    "url": "/fashion-cube/static/media/emptyCart.02800820.png"
+  },
+  {
+    "revision": "abebef23e38854b0a7ad161380471493",
+    "url": "/fashion-cube/static/media/empty_cart.abebef23.png"
+  },
+  {
+    "revision": "dc7970a3ddb8a96a0f4f74bd17d05c24",
+    "url": "/fashion-cube/static/media/slider_1.dc7970a3.jpg"
+  },
+  {
+    "revision": "a5bc9e893257dc0c821b846e32c7170d",
+    "url": "/fashion-cube/static/media/slider_2.a5bc9e89.jpg"
+  },
+  {
+    "revision": "035f02ed5a4bc5be22aff5b9e6a1c3d1",
+    "url": "/fashion-cube/static/media/slider_3.035f02ed.jpg"
+  }
+]);
